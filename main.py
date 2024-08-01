6aaf88f1aced0f8a213766d104dacfe142d72a38
Yusuf Can Akyol <canyusufakyol@gmail.com>
2024-08-01 23:26:55 +0300

    timer added

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from plyer import notification
+
+def send_notification(title, message):
+    notification.notify(
+        title=title,
+        message=message,
+        timeout=10
+    )
+
+if __name__ == "__main__":
+    title = "Desktop Notifier"
+    message = "This is a test notification."
+
+    send_notification(title, message)
